cd9e15c44977279c863759b7796aba76a0bfdc7e
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-08-25 18:00:34 -0500

    Updating tests

--- test/fabric/django.py
+++ test/fabric/django.py
@@ -8,7 +8,7 @@ def check():
     env.platform_family = detect.detect()
 
     assert package.installed("apache2")
-    assert file.exists("/srv/venv/bin/django-admin.py")
+    assert file.exists("/srv/workspace/bin/django-admin.py")
     assert file.exists("/etc/apache2/mods-enabled/wsgi.conf")
     assert file.exists("/etc/apache2/mods-enabled/wsgi.load")
     assert port.is_listening(80)
